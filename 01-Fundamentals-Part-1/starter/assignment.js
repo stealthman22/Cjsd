87fec50cc98a6177010a577eb00c49fed317a2fa
stealthman22 <tdotonline@gmail.com>
2021-07-30 20:06:34 +0000

    done with lecture 12

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,16 @@
+const country = 'Ghana';
+const continent = 'Africa';
+let population = 30420000;
+
+// console.log(country);
+// console.log(continent);
+//  console.log(population);
+
+ // Assignment for Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof(isIsland));
+console.log(typeof(language));
+console.log(typeof(country));
+console.log(typeof(language))
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,33 @@
+// let js = 'amazing';
+// // if(js === 'amazing') alert('js is fun');
+// console.log( 40+8+23-10);
+// console.log('Joel');
+
+// let firstName = 'Bobby';
+// console.log(firstName);
+
+// let PI = 3.1415
+
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+
+// console.log(myFirstJob)
+
+let iLoveJs = true
+console.log(iLoveJs);
+console.log(typeof(iLoveJs));
+console.log(typeof(true));
+console.log(typeof(21));
+console.log(typeof('Runaway'));
+
+
+iLoveJs = 'Lalalalala';
+console.log(typeof(iLoveJs))
+
+let work;
+work = 'Flight attendant'
+console.log(work);
+console.log(typeof(work))
+
+console.log(typeof(null))
+
