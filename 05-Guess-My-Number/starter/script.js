22ca332f5e580af1f3b08cebea9a4d31eac456b4
stealthman22 <tdotonline@gmail.com>
2021-10-13 23:08:01 +0000

    solved the not going lower that 19 problem

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Number BOys';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+// EVENT LISTENERS
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number ⁉️';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number 😹';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too High !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 😢';
+    }
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low !';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
