fd47eb95be9c27dc43be7ac60fce77354beb8b75
stealthman22 <tdotonline@gmail.com>
2021-10-19 22:27:58 +0000

    finished implementing player hold and win features ;

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -22,27 +22,66 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let currentPlayer = 0;
+const playerNow = document.querySelector(`.player--${currentPlayer}`);
+
+// game state varaible
+let playing = true;
+
+// switching players
+function switchPlayer() {
+  // Switch to next player
+  document.getElementById(`current--${currentPlayer}`).textContent = 0;
+  currentScore = 0;
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
 
 // Rolling the dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. Generate a random dice roll number
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-  // 2. Display dice number rolled
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-  // Check for rolled 1: if true, switch to next player
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    // Dynamically switch between current players
-    document.getElementById(`current--${currentPlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    document.getElementById(`current--${currentPlayer}`).textContent = 0;
-    currentScore = 0;
-    currentPlayer = currentPlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
+  if (playing) {
+    // 1. Generate a random dice roll number
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    // 2. Display dice number rolled
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    // Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      // Dynamically switch between current players
+      document.getElementById(`current--${currentPlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// Implementing player hold logic
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // Add current score to current player's total score
+
+    scores[currentPlayer] += currentScore;
+
+    // scores[1]= scores[1] + currentScore
+    document.getElementById(`score--${currentPlayer}`).textContent =
+      scores[currentPlayer];
+    //  Check if score is >= 100
+    if (scores[currentPlayer] >= 20) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      // FInish the game
+
+      playerNow.classList.add('player--winner');
+
+      playerNow.classList.remove('player--active');
+      console.log('Now playing', playerNow);
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
   }
 });
