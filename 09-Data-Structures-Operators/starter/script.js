39ac60dead4f533b4d857bd721ede2e9151dc565
stealthman22 <tdotonline@gmail.com>
2021-11-29 03:07:06 +0000

    learnt the nullish Coalescing Operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -44,6 +44,10 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Heare is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
 };
 
 // method calls
@@ -62,36 +66,36 @@ restaurant.orderDelivery({
 
 // Destructuring Objects
 
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
 
-// destruc objects with different var names
-const {
-  name: resturantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(resturantName, hours, tags);
+// // destruc objects with different var names
+// const {
+//   name: resturantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(resturantName, hours, tags);
 
-// Destruc objects with default values
-const { menu = [], starterMenu = [] } = restaurant;
-console.log(menu, starterMenu);
+// // Destruc objects with default values
+// const { menu = [], starterMenu = [] } = restaurant;
+// console.log(menu, starterMenu);
 
-//  Destructuring nested objects
-const {
-  fri: { open, close },
-} = openingHours;
-console.log(open, close);
+// //  Destructuring nested objects
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close);
 
-// Mutating variables when destructuring objects
+// // Mutating variables when destructuring objects
 
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
 
-// with objects you need to wrap the statement in patenthesis
-({ a, b } = obj);
-console.log(a, b);
+// // with objects you need to wrap the statement in patenthesis
+// ({ a, b } = obj);
+// console.log(a, b);
 
 // //  Destruc Arrays
 // const arr = [2, 3, 4];
@@ -127,28 +131,28 @@ console.log(a, b);
 // The Spread Operator
 
 // Without spread
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
 
-// With spread
-const newArr = [1, 2, ...arr];
-console.log(newArr);
+// // With spread
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
 
-const newMenu = [...restaurant.mainMenu, 'Gnocci', 'Amala'];
-console.log(newMenu);
+// const newMenu = [...restaurant.mainMenu, 'Gnocci', 'Amala'];
+// console.log(newMenu);
 
-// Creating shallow copy of array
-const mainMenuCOpy = [...restaurant.mainMenu];
+// // Creating shallow copy of array
+// const mainMenuCOpy = [...restaurant.mainMenu];
 
-// Join 2 arrays
+// // Join 2 arrays
 
-const menu2 = [...restaurant.mainMenu, ...restaurant.starterMenu];
-console.log('join array', menu2);
+// const menu2 = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log('join array', menu2);
 
-const str = 'Joel';
-const letters = [...str, '', 'S'];
-console.log(letters);
+// const str = 'Joel';
+// const letters = [...str, '', 'S'];
+// console.log(letters);
 
 // inputing data ourselves in spread
 // const ingredients = [
@@ -161,11 +165,85 @@ console.log(letters);
 // restaurant.orderPasta(...ingredients);
 
 // ES 2018 spread on objects
-const newRestaurant = { foundIn: 1998, ...restaurant, founder: 'Eyram' };
-console.log(newRestaurant);
+// const newRestaurant = { foundIn: 1998, ...restaurant, founder: 'Eyram' };
+// console.log(newRestaurant);
 
-// Create shallow object copy with spread, rather than using object assign
-const restaurantCOpy = { ...restaurant };
-restaurantCOpy.name = 'Chicken Inn';
-console.log(restaurantCOpy.name);
-console.log(restaurant.name);
+// // Create shallow object copy with spread, rather than using object assign
+// const restaurantCOpy = { ...restaurant };
+// restaurantCOpy.name = 'Chicken Inn';
+// console.log(restaurantCOpy.name);
+// console.log(restaurant.name);
+
+// //  The rest pattern
+// // WITH DESTRUCTURING
+// // Spread because ... is on right
+// const arr = [1, , ...[3, 4]];
+
+// //  Rest pattern  because ... is on left
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// // Spread and rest at the same time
+// const [pizza, , Fisotta, ...otherFoods] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+
+// console.log(otherFoods);
+
+// // Rest with Objects
+// const { sat, ...weekDays } = restaurant.openingHours;
+// console.log(weekDays);
+
+// // WITH  FUNC
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+// };
+
+// add(2, 3, 4, 5);
+// add(1, 2);
+// add(1, 12, 3, 4, 4, 5, 5, 66);
+
+// const x = [3, 4, 6];
+// // calling a function while using the spread operator to build params
+// add(...x);
+
+// restaurant.orderPizza('mushrooms', 'onion,', 'olives');
+// restaurant.orderPizza('mushrooms');
+
+// SHORT CIRCUITING
+
+console.log('----OR----');
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+
+console.log('----AND----');
+console.log(0 && 'Joel');
+console.log(9 && 'Joel');
+console.log('Hello' && 23 && null && 'Jonas');
+
+// Practical Example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushroom', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
+
+// Nullish Coalescing Operator
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log('nullish', guestCorrect);
