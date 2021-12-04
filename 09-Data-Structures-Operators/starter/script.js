48f2db365d8076b3ca6c4dfd48652df527c3a62e
stealthman22 <tdotonline@gmail.com>
2021-12-04 05:59:03 +0000

    done with optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -310,3 +310,30 @@ console.log(rest1, rest2);
 
 // with optional chaining
 console.log(restaurant.openingHours.mon?.open);
+
+// Multiple optional chaining
+console.log(restaurant.openingHours?.mon?.open);
+
+// Real world example of Optional chaining
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  // having an error with 0 value
+  const open = restaurant.openingHours[day]?.open ?? `closed`;
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// optional chaining with methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderKokonte?.(0, 1) ?? 'Method does not exist');
+
+// optional chaining with arrays
+const users = [{ name: 'Jonas', email: 'hellojoe.com' }];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+// without optional chaining
+if (users.length > 0) {
+  console.log(users[0].name);
+} else {
+  console.log('User array empty');
+}
