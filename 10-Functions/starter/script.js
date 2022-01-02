de5412714bcf1f9f649ba789849709a5d5afe716
stealthman22 <tdotonline@gmail.com>
2022-01-02 00:37:49 +0000

    done with default parameters lecture

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const bookings = [];
+// Default Parameters
+// You can use anything as default, even expressions
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = numPassengers > 5 ? 150 * numPassengers : 200 * numPassengers
+) {
+  // old way to do default parameters ES 5
+  // numPassengers = numPassengers || 1;
+  // price = price || 200;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 5);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 5);
