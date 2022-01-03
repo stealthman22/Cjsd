f8d54d75a9c7b59ac28243b675edabe6dc95de06
stealthman22 <tdotonline@gmail.com>
2022-01-03 10:54:05 +0000

    finished with default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -25,3 +25,39 @@ createBooking('LH123', 5);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 5);
+
+// How passing arguments really work
+const flight = 'LH234';
+
+const joel = {
+  name: 'Alara Joel',
+  passport: 234455677,
+};
+
+// building flight checking in function
+const checkIn = function (flightNum, passenger) {
+  // flight is a primitive type, and only contains tacopy off the property, not the original
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 234455677) {
+    alert('Checked In');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+
+// checkIn(flight, joel);
+console.log(flight);
+console.log(joel);
+
+// same as doing
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(joel);
+checkIn(flight, joel);
