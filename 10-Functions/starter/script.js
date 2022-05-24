f22c69824f3baf9ed4cebe019319143de8e155fb
stealthman22 <tdotonline@gmail.com>
2022-05-24 12:51:39 +0000

    working on call and bind

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,24 +1,24 @@
 'use strict';
 
-// const bookings = [];
-// // Default Parameters
-// // You can use anything as default, even expressions
-// const createBooking = function (
-//   flightNum,
-//   numPassengers = 1,
-//   price = numPassengers > 5 ? 150 * numPassengers : 200 * numPassengers
-// ) {
-//   // old way to do default parameters ES 5
-//   // numPassengers = numPassengers || 1;
-//   // price = price || 200;
-//   const booking = {
-//     flightNum,
-//     numPassengers,
-//     price,
-//   };
-//   console.log(booking);
-//   bookings.push(booking);
-// };
+const bookings = [];
+// Default Parameters
+// You can use anything as default, even expressions
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = numPassengers > 5 ? 150 * numPassengers : 200 * numPassengers
+) {
+  // old way to do default parameters ES 5
+  // numPassengers = numPassengers || 1;
+  // price = price || 200;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
 
 // createBooking('LH123');
 // createBooking('LH123', 5);
@@ -26,41 +26,41 @@
 // createBooking('LH123', 5);
 // createBooking('LH123', undefined, 5);
 
-// // How passing arguments really work
-// const flight = 'LH234';
+// How passing arguments really work
+const flight = 'LH234';
 
-// const joel = {
-//   name: 'Alara Joel',
-//   passport: 234455677,
-// };
+const joel = {
+  name: 'Alara Joel',
+  passport: 234455677,
+};
 
-// // building flight checking in function
-// const checkIn = function (flightNum, passenger) {
-//   // flight is a primitive type, and only contains tacopy off the property, not the original
-//   flightNum = 'LH999';
-//   passenger.name = 'Mr. ' + passenger.name;
+// building flight checking in function
+const checkIn = function (flightNum, passenger) {
+  // flight is a primitive type, and only contains tacopy off the property, not the original
+  flightNum = 'LH999';
+  passenger.name = `Mr. ${passenger.name}`;
 
-//   if (passenger.passport === 234455677) {
-//     alert('Checked In');
-//   } else {
-//     alert('Wrong Passport');
-//   }
-// };
+  if (passenger.passport === 234455677) {
+    alert('Checked In');
+  } else {
+    alert('Wrong Passport');
+  }
+};
 
-// // checkIn(flight, joel);
-// console.log(flight);
-// console.log(joel);
-
-// // same as doing
-// // const flightNum = flight;
-// // const passenger = jonas;
-
-// const newPassport = function (person) {
-//   person.passport = Math.trunc(Math.random() * 10000000);
-// };
-
-// newPassport(joel);
 // checkIn(flight, joel);
+console.log(flight);
+console.log(joel);
+
+// same as doing
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(joel);
+checkIn(flight, joel);
 
 //  Higher order functions
 const oneWord = function (str) {
@@ -77,7 +77,6 @@ const upperFirstWord = function (str) {
 const transformer = function (str, fn) {
   console.log(`Original string: ${str}`);
   console.log(`Transformed string: ${fn(str)}`);
-
   console.log(`Transformed by: ${fn.name}`);
 };
 
@@ -87,42 +86,42 @@ transformer('Javascript is the best', oneWord);
 //  all event handlers are essentially higher order functions
 // js uses callbacks all the time
 
-// const high5 = function () {
-//   console.log(':wave:');
-// };
+const high5 = function () {
+  console.log(':wave:');
+};
 
-// document.body.addEventListener('click', high5);
+document.body.addEventListener('click', high5);
 
-// // for each can accepts a higher order function too
-// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+// for each can accepts a higher order function too
+['Jonas', 'Martha', 'Adam'].forEach(high5);
 
 //  trying to write my own HOF
-// const canDrive = function (person) {
-//   if (person.age > 18 && person.age < 80) {
-//     console.log('You are of drivable age and qualify for a license test');
-//   } else if (person.age < 18) {
-//     console.log(
-//       'Sorry please you do not qualify for a license, you are too young'
-//     );
-//   } else {
-//     ('Sorry please you do not qualify for a license, you are above retirement age, but can apply for a driver instead');
-//   }
-// };
+const canDrive = function (person) {
+  if (person.age > 18 && person.age < 80) {
+    console.log('You are of drivable age and qualify for a license test');
+  } else if (person.age < 18) {
+    console.log(
+      'Sorry please you do not qualify for a license, you are too young'
+    );
+  } else {
+    ('Sorry please you do not qualify for a license, you are above retirement age, but can apply for a driver instead');
+  }
+};
 
-// const checkCitizen = function (ctz) {
-//   const normalize = ctz.toLowerCase();
-//   if (normalize !== 'usa') {
-//     console.log(
-//       'Please you are not a citizen, pick the resident permit form first!'
-//     );
-//   }
-// };
+const checkCitizen = function (ctz) {
+  const normalize = ctz.toLowerCase();
+  if (normalize !== 'usa') {
+    console.log(
+      'Please you are not a citizen, pick the resident permit form first!'
+    );
+  }
+};
 
-// const john = {
-//   age: 25,
-//   country: 'USA',
-// };
-// canDrive(john, checkCitizen);
+const john = {
+  age: 25,
+  country: 'USA',
+};
+canDrive(john, checkCitizen);
 
 // Functions returning functions
 // const greet = function (greeting) {
@@ -164,6 +163,7 @@ const eurowings = {
 };
 
 const book = lufthansa.book;
+// console.log(book.this);
 
 // does not work
 // book(23, 'Sarah Willyboy');
@@ -218,21 +218,21 @@ buyButton.addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
 // Partial application with the bind method
 
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
 
-// now lets take care of a special tax case, one that we always know the rate
-const addVAT = addTax.bind(null, 0.23);
+// // now lets take care of a special tax case, one that we always know the rate
+// const addVAT = addTax.bind(null, 0.23);
 
-// same as
-// addTax = value => value + value * 0.23;
-console.log(addVAT(100));
+// // same as
+// // addTax = value => value + value * 0.23;
+// console.log(addVAT(100));
 
-// challenge: use the function returning functions pattern to mimic addTax function
-const calcVAT =
-  (rate = 0.23) =>
-  value =>
-    value + value * rate;
+// // challenge: use the function returning functions pattern to mimic addTax function
+// const calcVAT =
+//   (rate = 0.23) =>
+//   value =>
+//     value + value * rate;
 
-const finalCalcVAT = calcVAT();
-console.log(finalCalcVAT(100));
+// const finalCalcVAT = calcVAT();
+// console.log(finalCalcVAT(100));
